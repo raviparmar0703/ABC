4fe5e6d06b8c3c283c28c8211573e02fc8fb7963
raviparmar0703 <raviparmar0703@gmail.com>
2022-11-20 19:28:32 +0530

    add co

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/Navbar.js';
+import AddEmployee from './components/AddEmployee';
+
+function App() {
+  return (
+  <>
+    <Navbar/>
+    <AddEmployee/>
+    </>
+  );
+};
+
+export default App;
